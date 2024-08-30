b192cd6ffd90d62eab822b96b787a73f3775dd61
likesh k <likesh.k19@gmail.com>
2024-08-30 21:30:20 +0530

    Update hello.cpp

--- C++/hello.cpp
+++ C++/hello.cpp
@@ -4,3 +4,19 @@ int main() {
     std::cout << "Hello World!";
     return 0;
 }
+<<<<<<< Tabnine <<<<<<<
+/**//+
+ * @brief The main function of the program.//+
+ *//+
+ * This function is the entry point of the program. It prints "Hello World!" to the standard output and returns 0 to indicate successful program execution.//+
+ *//+
+ * @return An integer representing the exit status of the program. A return value of 0 indicates successful execution.//+
+ *///+
+int main() {//+
+    std::cout << "Hello World!";//+
+    return 0;//+
+}//+
+>>>>>>> Tabnine >>>>>>>
+
+
+

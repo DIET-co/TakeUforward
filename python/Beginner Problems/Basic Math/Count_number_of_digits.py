d4b7d1ce45ab435e5de654c916732806b4ac4871
likesh k <k.likesh27@gmail.com>
2025-01-11 21:36:12 +0530

    ----

---
+++ python/Beginner Problems/Basic Math/Count_number_of_digits.py
@@ -0,0 +1,30 @@
+class Solution:
+    # Function to count all digits in n
+    def countDigit(self, n):
+        # Edge case
+        if n == 0:
+            return 1
+
+        # Set counter to 0
+        cnt = 0
+
+        # Iterate until n is greater than zero
+        while n > 0:
+            # Extract last digit
+            lastDigit = n % 10
+
+            # Increment count of digits
+            cnt = cnt + 1
+            n = n // 10
+
+        return cnt
+
+# Input number
+n = 6678
+
+# Creating an instance of Solution class
+sol = Solution()
+
+# Function call to get count of digits in n
+ans = sol.countDigit(n)
+print(f"The count of digits in the given number is: {ans}")
